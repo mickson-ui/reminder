e2c2302031afefd459d7937b380406a8fc1c4628
Mickson Bonsu Bossman <mbonsu69@outlook.com>
2024-02-28 20:13:28 +0100

    Initial Commit

---
+++ reminder/reminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  reminderApp.swift
+//  reminder
+//
+//  Created by Mickson Bossman on 28.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct reminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
